f8e0e8a30bf6e756c7969cca1b774820e5d4d7fa
Francis <francispham89@gmail.com>
2018-05-15 15:16:33 -0700

    Done with Project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import stmedispa from '../img/stmedispa.png'
+import ninja from '../img/ninja.png'
+import stmedi from '../img/stmedi.png'
+
+
 const IndexPage = () => (
   <div>
     <h2>My Projects</h2>
     <ul style = {{
-    listStyle: 'none'
+    listStyle: 'none',
+    padding: '10px'
       }}>
       <li>
-        <a  href = 'https://github.com/phongpt1989/drill-ninjaz'
-              target = '_black'>Drillz Ninja
+        <a  href = 'https://github.com/phongpt1989/drill-ninjaz' target = '_black'>
+          <img src={ninja} alt='ninja'/>
+          <p>➣ <strong>Drillz Ninja</strong> built with Ruby, Ruby on Rails, PostgreSQL,
+            Bootstrap, HTML, CSS, JavaScript</p>
+          <hr/>
         </a>
-        <img/>
       </li>
-      <li><a  href = 'https://github.com/phongpt1989/Client_management_app'
-              target = '_black'>Client Management App</a></li>
-      <li><a  href = 'https://github.com/phongpt1989/final-project'
-              target = '_black'>St Medi Web App</a></li>
+      <li>
+        <a  href = 'https://github.com/phongpt1989/Client_management_app' target = '_black'>
+          <img src={stmedispa} alt='stmedispa'/>
+          <p>➣ <strong>Client Management App</strong> built with Ruby on Rails, PostgreSQL, HTML,
+            CSS, JavaScript</p>
+          <hr/>
+        </a>
+      </li>
+      <li>
+        <a  href = 'https://github.com/phongpt1989/final-project' target = '_black'>
+          <img src={stmedi} alt='stmedi'/>
+          <p>➣ <strong>St Medi Web App</strong> built with React.js, Redux.js, Express.js,
+            MongoDB, HTML, CSS, JavaScrip</p>
+        </a>
+      </li>
     </ul>
   </div>
 )
