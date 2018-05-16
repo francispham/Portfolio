58da8bf31af24d251a2e5927aa40e10d325ffbfc
Francis <francispham89@gmail.com>
2018-05-16 12:50:11 -0700

    Fixed Contact Bug 1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,49 +1,74 @@
-import React from 'react'
+import React from "react";
+import { navigateTo } from "gatsby-link";
 
-const FooterLinks = [
-  {
-    name: 'Github',
-    link: 'https://github.com/phongpt1989'
-  },{
-    name: 'LinkedIn',
-    link: 'http://linkedin.com/in/francispham89'
-  },{
-    name: 'Gmail',
-    link: 'fransicpham89@gmail.com'
-  }
-]
-
-const Contact = () => {
-  return (
-    <div>
-      <h2>CONTACT</h2>
-      <p>I am available for hire and open to any ideas of cooperation.</p>
-      <p>More Questions? Please send me a message, Thank you!</p>
-
-      <form className="form flex" name ="ContactMe" method ="POST" action="/thankyou"
-            data-netlify="true" netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field" />
-        <input name = "name" placeholder= "Your Name" type = "text" />
-        <input name = "email" placeholder = "name@name.com" type = "email" />
-        <textarea name = 'message' placeholder = "Your Message" />
-        <input className="button" type="submit" value="Send Message"/>
-      </form>
-
-      <br/>
-      <hr/>
-
-      <div style = {{display: 'flex', justifyContent:'center'}}>
-        {FooterLinks.map(link => (
-          <div className="social" style = {{marginRight: '2em'}}>
-            <a target = "_blank" rel = "nofollow" className = {link.name.toLowerCase()} href = {link.link}>
-              {link.name}
-            </a>
-          </div>
-        ))}
-      </div>
-      <br/>
-    </div>
-  )
+function encode(data) {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
 
-export default Contact
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => navigateTo('/thankyou/'))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thankyou/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+            <input type="text" name="name" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
