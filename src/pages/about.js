be15bbd55ab2278947a6fa34a357b3e5d8dc963d
Francis <francispham89@gmail.com>
2018-06-06 17:18:24 -0700

    Fixed some words

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ const About = () => {
       <h2>ABOUT</h2>
       <p>Graduated from CodeCore Full Stack Web Development Program.</p>
       <p>Currently working as a freelancer, and also available for hire.</p>
-      <p>Specialized in <strong> Rudy on Rails, Node.js, PostgresQL, React.js, Bootstrap,
+      <p>Specialized in <strong> Ruby on Rails, Node.js, PostgresQL, React.js, Bootstrap,
          HTML and CSS </strong> </p>
       <p>Since the sky is limitless, I am always ready for new challenges...</p>
     </div>
